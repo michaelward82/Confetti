75f9f4e2a0c8e7a48c7003397463bf65bd780a4d
Michael Ward <m.ward@leythers.com>
2011-08-12 00:35:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Confetti::Application.config.secret_token = '9bcd7d8bb6905ba96e211b891929b8b5bc96399bc6587fdd8e28dfcd962e2f87e32fc666a87b2231c05ba0cb69ec84061aca3e31c4015cc74d6289196b5bd031'
