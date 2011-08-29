c2b1e3aa858fac7bcb43cc4d1cc4b4c1acd53a37
Michael Ward <m.ward@leythers.com>
2011-08-29 01:03:44 +0100

    Rollback to rack-pagespeed

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'rack/pagespeed'
+require 'tmpdir'
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
